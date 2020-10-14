fa9d07823f649208c49f96c6866c91725ab27a05
Jesús Herrera <jesusherrera13@gmail.com>
2020-10-14 06:26:21 -0600

    Login cambio de título

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Elecciones | Log in</title>
+  <title>Rutas | Log in</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -21,7 +21,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="index2.html"><b>Elecc</b>iones</a>
+    <a href="index2.html"><b>Ru</b>as</a>
   </div>
   <!-- /.login-logo -->
   <div class="card">
