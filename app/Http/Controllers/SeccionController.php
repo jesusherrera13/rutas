ababab23497395c20c04fbdeb601bfdb5cc08ffd
Jesús Herrera <jesusherrera13@gmail.com>
2021-02-16 15:37:39 -0700

    Casillas seeder solo basica

--- app/Http/Controllers/SeccionController.php
+++ app/Http/Controllers/SeccionController.php
@@ -246,7 +246,7 @@ class SeccionController extends Controller
                 $casilla->save();
             }
 
-            for($i = 1; $i <= 10; $i++) {
+            /* for($i = 1; $i <= 10; $i++) {
 
                 $id_tipo_casilla = 'C';
 
@@ -265,7 +265,7 @@ class SeccionController extends Controller
 
                     $casilla->save();
                 }
-            }
+            } */
 
             $id_tipo_casilla = 'E';
 
