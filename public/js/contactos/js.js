374df2085e3821ba91417376fdc6799e3819c854
Jesús Herrera <jesusherrera13@gmail.com>
2021-02-18 13:05:07 -0700

    Contactos export pdf font size

--- public/js/contactos/js.js
+++ public/js/contactos/js.js
@@ -1073,8 +1073,8 @@ function impresionFormatos() {
                         pageSize: 'A4',
                         customize : function(doc) {
                             doc.pageMargins = [100, 10, 10,10 ]; 
-                            doc.styles.tableHeader.fontSize = 9;
-                            doc.defaultStyle.fontSize = 9;
+                            doc.styles.tableHeader.fontSize = 8;
+                            doc.defaultStyle.fontSize = 8;
                         },
                         exportOptions: {
                             // columns: [ 0, 1, 2, 4, 5, 6, 7, 8, 9 ]
